1a900f08f89041ba869d9961729dbda77c88002f
AdemirDFilho <167543529+AdemirDFilho@users.noreply.github.com>
2025-09-22 12:10:05 -0300

    Add files via upload

---
+++ FLOT_vs_INT_DECIMAIS.py
@@ -0,0 +1,96 @@
+print("ola mundo")
+print("INT VS FLOAT PARA REPRESENTAR NÚMERO DECECIMAIS/DINHEIRO $$")
+print(1+1)
+
+print(0.1+0.1)
+print(0.1+0.2)
+print(0.1+0.3)
+print(0.1+0.4)
+print(0.1+0.5)
+print(0.1+0.6)
+print(0.1+0.7)
+print(0.1+0.8)
+print(0.1+0.9)
+print(0.1+1)
+
+num1 = 0.1
+num2 = 0.7
+num3 = 1
+num4 = 7
+print(num1 + num2)
+
+print("_____________________________")
+print(num1)
+print(num2)
+print(num3)
+print(num4)
+print(f"SOMA PRECOS 1 E 2 = R${num1+num2}")
+print(f"SOMA PRECOS 3 E 4 = R${(num3+num4)/100}")
+
+print("____________EXE1-INPUT-PRECOS_________________")
+print("___________ FLOAT_______________")
+preco1 = float(input("Preço 1 é R$ "))
+preco2 = float(input("Preço 2 é R$ "))
+print("___________ INT_______________")
+preco3 = int(input("Preço 3 é R$ "))
+preco4 = int(input("Preço 4 é R$ "))
+
+print(preco1)
+print(preco2)
+print(preco3)
+print(preco4)
+
+print("...........SOMA PRECOS 1 E 2................")
+print(preco1+preco2)
+print("...........SOMA PRECOS 3 E 4................")
+print(preco3+preco4)
+print("...........SOMA PRECOS 1 E 3................")
+print(preco1+preco3)
+print("...........SOMA PRECOS 2 E 4................")
+print(preco2+preco4)
+print("...........SOMA PRECOS 1 + 2 + 3 + 4................")
+print(preco1+preco2+preco3+preco4)
+print("____________EXE1-INPUT-PRECOS_________________")
+
+total = preco1+preco2+preco3+preco4
+print(total)
+
+if total <= 0:
+    print("valor nulo")
+else:
+    print("OK")
+
+#if
+#elif
+
+#and or
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
