eccf3c78ec3fdb68ccea48a58837e37eea820870
AdemirDFilho <167543529+AdemirDFilho@users.noreply.github.com>
2025-11-12 09:53:22 -0300

    Add files via upload

---
+++ Disqus_Mbit.py
@@ -0,0 +1,10 @@
+#Disqus_Mbit.py
+#Acessar comentários de posts do blog MeioBit.com via central de comentarios do Disqus.
+#Útil para acessar comentários de posts antigos, cujos comentários disqus não "carregam" mais no site.
+
+# Disqus_Mbit = https://disqus.com/home/discussion/mbit/titulo_do_artigo_em_minusculas_sem_caractere_especial
+# Se não me falha a memória, casos em que link e título divergem, prevalece o título. 
+
+# EXEMPLO:
+# LINK POST: https://meiobit.com/434479/perseverance_envia_os_mais_incriveis_videos_de_marte/
+# LINK DISQUS: https://disqus.com/home/discussion/mbit/perseverance_envia_os_mais_incriveis_videos_de_marte
