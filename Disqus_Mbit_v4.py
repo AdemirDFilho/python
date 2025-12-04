dabbfede95ef9ecd87c2e8ba0e503d941eb522b5
AdemirDFilho <167543529+AdemirDFilho@users.noreply.github.com>
2025-12-04 17:56:10 -0300

    Add files via upload
    # v4 -> Abre o link no navegador padrão.
    # v4 -> Convertido em .exe via pyinstaller. 
    # v4 -> pyinstaller --onefile Disqus_Mbit_v4.py

---
+++ Disqus_Mbit_v4.py
@@ -0,0 +1,53 @@
+print("Disqus_Mbit")
+# v4 -> Abre a URL no navegador padrão.
+
+# Acessar comentários de posts do blog MeioBit.com via central de comentarios do Disqus.
+# Útil para acessar comentários de posts antigos, cujos comentários disqus não "carregam" mais no site.
+
+# Disqus_Mbit = https://disqus.com/home/discussion/mbit/titulo_do_artigo_em_minusculas_sem_caractere_especial
+# Se não me falha a memória, casos em que link e título divergem, prevalece o título. 
+
+# EXEMPLO:
+# LINK POST: https://meiobit.com/434479/perseverance-envia-os-mais-incriveis-videos-de-marte/
+# LINK DISQUS: https://disqus.com/home/discussion/mbit/perseverance_envia_os_mais_incriveis_videos_de_marte
+
+import re
+import unicodedata
+import webbrowser
+
+def slugify(texto: str) -> str:
+    # Converte para minúsculas
+    texto = texto.lower()
+
+    # Remove acentos
+    texto = unicodedata.normalize('NFD', texto)
+    texto = ''.join(c for c in texto if unicodedata.category(c) != 'Mn')
+
+    # Mantém apenas letras, números e espaços
+    texto = re.sub(r'[^a-z0-9\s-]', '', texto)
+
+    # Substitui espaços por underline
+    texto = re.sub(r'\s+', '_', texto)
+
+    # Substitui HÍFENS por underline
+    texto = re.sub(r'\-+', '_', texto)
+
+    # Remove múltiplos underlines
+    texto = re.sub(r'_+', '_', texto)
+
+    # Remove underline no início ou final
+    texto = texto.strip('_')
+
+    return texto
+
+while True:
+    texto = input('Cole o título do artigo MeioBit ... ')
+    #print(slugify(texto))
+
+    url_disqus = slugify(texto)
+    link = (f"https://disqus.com/home/discussion/mbit/{url_disqus}")
+    print(url_disqus)
+    print(link)
+    webbrowser.open(link)
+
+#Obrigado chatGPT!
